712760adc49863e3fda2c589ae590a5579e6c709
Mike Kroutikov <mkroutikov@innodata.com>
2019-06-18 15:54:12 -0400

    cleanup of the demo code

--- sane_demo.py
+++ sane_demo.py
@@ -8,310 +8,107 @@ from maskrcnn_benchmark.modeling.roi_heads.mask_head.inference import Masker
 from maskrcnn_benchmark.structures.image_list import to_image_list
 from fcos_model import FCOSModel
 from fcos_post_processor import FCOSPostProcessor
-
+import contextlib
 import time
 
 
-class COCODemo(object):
+@contextlib.contextmanager
+def timeit(title='Time elapsed'):
+    start = time.time()
+    try:
+        yield
+    finally:
+        print('%s: %.2fs' % (title, (time.time()-start)))
 
-    def __init__(
-        self,
-        checkpoint,
-        confidence_thresholds_for_classes,
-        show_mask_heatmaps=False,
-        masks_per_dim=2,
-        min_image_size=224,
-        device='cpu',
-        input_to_bgr255=True,
-        input_pixel_mean = (102.9801, 115.9465, 122.7717),
-        input_pixel_std = [1., 1., 1.],
-        size_divisibility=32,
-        draw_masks = False,
-        draw_keypoints = False,
-        inference_th=0.05,
-        pre_nms_top_n=1000,
-        nms_th=0.6,
-        fpn_post_nms_top_n=100,
-    ):
-        self.model = FCOSModel(num_classes=1)
-        self.model.eval()
 
-        self.box_selector = FCOSPostProcessor(
-            pre_nms_thresh=inference_th,
-            pre_nms_top_n=pre_nms_top_n,
-            nms_thresh=nms_th,
-            fpn_post_nms_top_n=fpn_post_nms_top_n,
-            min_size=0,
-            num_classes=2  # here we count background??? -MK
+def build_transform(convert_to_bgr255=True, pixel_mean=(102.9801, 115.9465, 122.7717), pixel_std=(1., 1., 1.), min_image_size=224):
+    """
+    Creates a basic transformation that was used to train the models
+    """
+
+    # we are loading images with OpenCV, so we don't need to convert them
+    # to BGR, they are already! So all we need to do is to normalize
+    # by 255 if we want to convert to BGR255 format, or flip the channels
+    # if we want it to be in RGB in [0-1] range.
+    if convert_to_bgr255:
+        to_bgr_transform = T.Lambda(lambda x: x * 255)
+    else:
+        to_bgr_transform = T.Lambda(lambda x: x[[2, 1, 0]])
+
+    normalize_transform = T.Normalize(mean=pixel_mean, std=pixel_std)
+
+    transform = T.Compose(
+        [
+            T.ToPILImage(),
+            T.Resize(min_image_size),
+            T.ToTensor(),
+            to_bgr_transform,
+            normalize_transform,
+        ]
+    )
+    return transform
+
+
+def compute_colors_for_labels(labels):
+    """
+    Simple function that adds fixed colors depending on the class
+    """
+    palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+    colors = labels[:, None] * palette
+    colors = (colors % 255).numpy().astype("uint8")
+    return colors
+
+
+def overlay_boxes(image, predictions):
+    """
+    Adds the predicted boxes on top of the image
+
+    Arguments:
+        image (np.ndarray): an image as returned by OpenCV
+        predictions (BoxList): the result of the computation by the model.
+            It should contain the field `labels`.
+    """
+    labels = predictions.get_field("labels")
+    boxes = predictions.bbox
+
+    colors = compute_colors_for_labels(labels).tolist()
+
+    for box, color in zip(boxes, colors):
+        box = box.to(torch.int64)
+        top_left, bottom_right = box[:2].tolist(), box[2:].tolist()
+        image = cv2.rectangle(
+            image, tuple(top_left), tuple(bottom_right), tuple(color), 1
         )
 
-        self.device = torch.device(device)
-        self.model.to(self.device)
-        self.min_image_size = min_image_size
+    return image
 
-        from maskrcnn_benchmark.utils.checkpoint import Checkpointer
-        checkpointer = Checkpointer(self.model, save_dir='')
-        _ = checkpointer.load(checkpoint)
+def overlay_class_names(image, predictions):
+    """
+    Adds detected class names and scores in the positions defined by the
+    top-left corner of the predicted bounding box
 
-        self.transforms = self.build_transform(input_to_bgr255, input_pixel_mean, input_pixel_std)
+    Arguments:
+        image (np.ndarray): an image as returned by OpenCV
+        predictions (BoxList): the result of the computation by the model.
+            It should contain the field `scores` and `labels`.
+    """
+    scores = predictions.get_field("scores").tolist()
+    labels = predictions.get_field("labels").tolist()
+    labels = ['block' for i in labels]
+    boxes = predictions.bbox
 
-        mask_threshold = -1 if show_mask_heatmaps else 0.5
-        self.masker = Masker(threshold=mask_threshold, padding=1)
-
-        # used to make colors for each class
-        self.palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
-
-        self.cpu_device = torch.device("cpu")
-        self.confidence_thresholds_for_classes = torch.tensor(confidence_thresholds_for_classes)
-        self.show_mask_heatmaps = show_mask_heatmaps
-        self.masks_per_dim = masks_per_dim
-        self.size_divisibility = size_divisibility
-        self.draw_masks = draw_masks
-        self.draw_keypoints = draw_keypoints
-
-    def build_transform(self, convert_to_bgr255, pixel_mean, pixel_std):
-        """
-        Creates a basic transformation that was used to train the models
-        """
-
-        # we are loading images with OpenCV, so we don't need to convert them
-        # to BGR, they are already! So all we need to do is to normalize
-        # by 255 if we want to convert to BGR255 format, or flip the channels
-        # if we want it to be in RGB in [0-1] range.
-        if convert_to_bgr255:
-            to_bgr_transform = T.Lambda(lambda x: x * 255)
-        else:
-            to_bgr_transform = T.Lambda(lambda x: x[[2, 1, 0]])
-
-        normalize_transform = T.Normalize(mean=pixel_mean, std=pixel_std)
-
-        transform = T.Compose(
-            [
-                T.ToPILImage(),
-                T.Resize(self.min_image_size),
-                T.ToTensor(),
-                to_bgr_transform,
-                normalize_transform,
-            ]
+    template = "{}: {:.2f}"
+    for box, score, label in zip(boxes, scores, labels):
+        x, y = box[:2]
+        s = template.format(label, score)
+        cv2.putText(
+            image, s, (x+1, y+1), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 3
         )
-        return transform
-
-    def run_on_opencv_image(self, image):
-        """
-        Arguments:
-            image (np.ndarray): an image as returned by OpenCV
-
-        Returns:
-            prediction (BoxList): the detected objects. Additional information
-                of the detection properties can be found in the fields of
-                the BoxList via `prediction.fields()`
-        """
-        predictions = self.compute_prediction(image)
-        top_predictions = self.select_top_predictions(predictions)
-
-        result = image.copy()
-        if self.show_mask_heatmaps:
-            return self.create_mask_montage(result, top_predictions)
-        result = self.overlay_boxes(result, top_predictions)
-        if self.draw_masks:
-            result = self.overlay_mask(result, top_predictions)
-        if self.draw_keypoints:
-            result = self.overlay_keypoints(result, top_predictions)
-        result = self.overlay_class_names(result, top_predictions)
-
-        return result
-
-    def compute_prediction(self, original_image):
-        """
-        Arguments:
-            original_image (np.ndarray): an image as returned by OpenCV
-
-        Returns:
-            prediction (BoxList): the detected objects. Additional information
-                of the detection properties can be found in the fields of
-                the BoxList via `prediction.fields()`
-        """
-        # apply pre-processing to image
-        image = self.transforms(original_image)
-        # convert to an ImageList, padded so that it is divisible by
-        image_list = to_image_list(image, self.size_divisibility)
-        image_list = image_list.to(self.device)
-        # compute predictions
-        with torch.no_grad():
-            logits = self.model(image_list)
-            predictions = self.box_selector(logits, image_list.image_sizes)
-
-        predictions = [o.to(self.cpu_device) for o in predictions]
-
-        # always single image is passed at a time
-        assert len(predictions) == 1
-        prediction = predictions[0]
-
-        # reshape prediction (a BoxList) into the original image size
-        height, width = original_image.shape[:-1]
-        prediction = prediction.resize((width, height))
-
-        if prediction.has_field("mask"):
-            # if we have masks, paste the masks in the right position
-            # in the image, as defined by the bounding boxes
-            masks = prediction.get_field("mask")
-            # always single image is passed at a time
-            masks = self.masker([masks], [prediction])[0]
-            prediction.add_field("mask", masks)
-        return prediction
-
-    def select_top_predictions(self, predictions):
-        """
-        Select only predictions which have a `score` > self.confidence_threshold,
-        and returns the predictions in descending order of score
-
-        Arguments:
-            predictions (BoxList): the result of the computation by the model.
-                It should contain the field `scores`.
-
-        Returns:
-            prediction (BoxList): the detected objects. Additional information
-                of the detection properties can be found in the fields of
-                the BoxList via `prediction.fields()`
-        """
-        scores = predictions.get_field("scores")
-        labels = predictions.get_field("labels")
-        thresholds = self.confidence_thresholds_for_classes[(labels - 1).long()]
-        keep = torch.nonzero(scores > thresholds).squeeze(1)
-        predictions = predictions[keep]
-        scores = predictions.get_field("scores")
-        _, idx = scores.sort(0, descending=True)
-        return predictions[idx]
-
-    def compute_colors_for_labels(self, labels):
-        """
-        Simple function that adds fixed colors depending on the class
-        """
-        colors = labels[:, None] * self.palette
-        colors = (colors % 255).numpy().astype("uint8")
-        return colors
-
-    def overlay_boxes(self, image, predictions):
-        """
-        Adds the predicted boxes on top of the image
-
-        Arguments:
-            image (np.ndarray): an image as returned by OpenCV
-            predictions (BoxList): the result of the computation by the model.
-                It should contain the field `labels`.
-        """
-        labels = predictions.get_field("labels")
-        boxes = predictions.bbox
-
-        colors = self.compute_colors_for_labels(labels).tolist()
-
-        for box, color in zip(boxes, colors):
-            box = box.to(torch.int64)
-            top_left, bottom_right = box[:2].tolist(), box[2:].tolist()
-            image = cv2.rectangle(
-                image, tuple(top_left), tuple(bottom_right), tuple(color), 1
-            )
-
-        return image
-
-    def overlay_mask(self, image, predictions):
-        """
-        Adds the instances contours for each predicted object.
-        Each label has a different color.
-
-        Arguments:
-            image (np.ndarray): an image as returned by OpenCV
-            predictions (BoxList): the result of the computation by the model.
-                It should contain the field `mask` and `labels`.
-        """
-        masks = predictions.get_field("mask").numpy()
-        labels = predictions.get_field("labels")
-
-        colors = self.compute_colors_for_labels(labels).tolist()
-
-        for mask, color in zip(masks, colors):
-            thresh = mask[0, :, :, None]
-            contours, hierarchy = cv2_util.findContours(
-                thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-            )
-            image = cv2.drawContours(image, contours, -1, color, 3)
-
-        composite = image
-
-        return composite
-
-    def overlay_keypoints(self, image, predictions):
-        keypoints = predictions.get_field("keypoints")
-        kps = keypoints.keypoints
-        scores = keypoints.get_field("logits")
-        kps = torch.cat((kps[:, :, 0:2], scores[:, :, None]), dim=2).numpy()
-        for region in kps:
-            image = vis_keypoints(image, region.transpose((1, 0)))
-        return image
-
-    def create_mask_montage(self, image, predictions):
-        """
-        Create a montage showing the probability heatmaps for each one one of the
-        detected objects
-
-        Arguments:
-            image (np.ndarray): an image as returned by OpenCV
-            predictions (BoxList): the result of the computation by the model.
-                It should contain the field `mask`.
-        """
-        masks = predictions.get_field("mask")
-        masks_per_dim = self.masks_per_dim
-        masks = L.interpolate(
-            masks.float(), scale_factor=1 / masks_per_dim
-        ).byte()
-        height, width = masks.shape[-2:]
-        max_masks = masks_per_dim ** 2
-        masks = masks[:max_masks]
-        # handle case where we have less detections than max_masks
-        if len(masks) < max_masks:
-            masks_padded = torch.zeros(max_masks, 1, height, width, dtype=torch.uint8)
-            masks_padded[: len(masks)] = masks
-            masks = masks_padded
-        masks = masks.reshape(masks_per_dim, masks_per_dim, height, width)
-        result = torch.zeros(
-            (masks_per_dim * height, masks_per_dim * width), dtype=torch.uint8
+        cv2.putText(
+            image, s, (x, y), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 128), 1
         )
-        for y in range(masks_per_dim):
-            start_y = y * height
-            end_y = (y + 1) * height
-            for x in range(masks_per_dim):
-                start_x = x * width
-                end_x = (x + 1) * width
-                result[start_y:end_y, start_x:end_x] = masks[y, x]
-        return cv2.applyColorMap(result.numpy(), cv2.COLORMAP_JET)
 
-    def overlay_class_names(self, image, predictions):
-        """
-        Adds detected class names and scores in the positions defined by the
-        top-left corner of the predicted bounding box
-
-        Arguments:
-            image (np.ndarray): an image as returned by OpenCV
-            predictions (BoxList): the result of the computation by the model.
-                It should contain the field `scores` and `labels`.
-        """
-        scores = predictions.get_field("scores").tolist()
-        labels = predictions.get_field("labels").tolist()
-        labels = ['block' for i in labels]
-        boxes = predictions.bbox
-
-        template = "{}: {:.2f}"
-        for box, score, label in zip(boxes, scores, labels):
-            x, y = box[:2]
-            s = template.format(label, score)
-            cv2.putText(
-                image, s, (x+1, y+1), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 3
-            )
-            cv2.putText(
-                image, s, (x, y), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 128), 1
-            )
-
-        return image
+    return image
 
 
 def main():
@@ -367,23 +164,55 @@ def main():
         0.1730124056339264, 0.1857597529888153
     ]
 
-    demo_im_names = os.listdir(args.images_dir)
+    state_dict = torch.load(args.checkpoint, map_location='cpu')
+    model = FCOSModel(num_classes=1)
+    model.load_state_dict(state_dict['model'])
+    model.eval()
 
-    # prepare object that handles inference plus adds predictions on top of image
-    coco_demo = COCODemo(
-        args.checkpoint,
-        confidence_thresholds_for_classes=thresholds_for_classes,
-        min_image_size=800,
+    box_selector = FCOSPostProcessor(
+        pre_nms_thresh=0.05,
+        pre_nms_top_n=1000,
+        nms_thresh=0.6,
+        fpn_post_nms_top_n=100,
+        min_size=0,
+        num_classes=2  # here we count background??? -MK
     )
 
-    for im_name in demo_im_names:
+    transform = build_transform(min_image_size=800)
+
+    for im_name in os.listdir(args.images_dir):
         img = cv2.imread(os.path.join(args.images_dir, im_name))
         if img is None:
             continue
-        start_time = time.time()
-        composite = coco_demo.run_on_opencv_image(img)
-        print("{}\tinference time: {:.2f}s".format(im_name, time.time() - start_time))
+
+        with timeit('%s\tinference time' % im_name):
+            # convert to an ImageList, padded so that it is divisible by
+            image_list = to_image_list(transform(img), 32)
+
+            # compute predictions
+            with torch.no_grad():
+                logits = model(image_list)
+                # always single image is passed at a time
+                prediction = box_selector(logits, image_list.image_sizes)[0]
+
+            # reshape prediction (a BoxList) into the original image size
+            height, width = img.shape[:-1]
+            prediction = prediction.resize((width, height))
+
+            scores = prediction.get_field("scores")
+            labels = prediction.get_field("labels")
+            thresholds = torch.tensor(thresholds_for_classes)[(labels - 1).long()]
+            keep = torch.nonzero(scores > thresholds).squeeze(1)
+            prediction = prediction[keep]
+            scores = prediction.get_field("scores")
+            _, idx = scores.sort(0, descending=True)
+            top_predictions = prediction[idx]
+
+            composite = overlay_boxes(img.copy(), top_predictions)
+            composite = overlay_class_names(composite, top_predictions)
+
         cv2.imshow(im_name, composite)
+
     print("Press any keys to exit ...")
     cv2.waitKey()
     cv2.destroyAllWindows()
