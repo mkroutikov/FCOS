2a9efb3b51354d47c517f0507fbd967848862dc5
Mike Kroutikov <mkroutikov@innodata.com>
2019-06-19 06:42:57 -0400

    introduced single-block training mode

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -2,6 +2,5 @@
 from .coco import COCODataset
 from .voc import PascalVOCDataset
 from .concat_dataset import ConcatDataset
-from .scarlet import Scarlet300Dataset
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset", "Scarlet300Dataset"]
+__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset"]
